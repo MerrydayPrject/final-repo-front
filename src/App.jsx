5ebcbe9442287eedac7ad350eaec754f8b6b411d
seoyoung <gu05087@gmail.com>
2025-11-26 16:04:30 +0900

    IOS/AOS 분리

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainPage from './pages/Main/MainPage'
 import GeneralFitting from './pages/General/GeneralFitting'
 import CustomFitting from './pages/Custom/CustomFitting'
 import BodyAnalysis from './pages/Analysis/BodyAnalysis'
+import { addPlatformClasses } from './utils/platform'
 import './styles/App.css'
 
 function App() {
@@ -11,6 +12,21 @@ function App() {
 
     // 일반피팅 페이지로 전달할 카테고리
     const [selectedCategoryForFitting, setSelectedCategoryForFitting] = useState(null)
+
+    // 플랫폼 감지 및 클래스 추가
+    useEffect(() => {
+        addPlatformClasses()
+
+        // 화면 크기 변경 시에도 플랫폼 클래스 업데이트
+        const handleResize = () => {
+            addPlatformClasses()
+        }
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     // 새로고침 시 스크롤을 최상단으로 이동
     useEffect(() => {
